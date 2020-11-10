d78abce73be8ccacca90b2166ca46074c5aead75
Andres Echeverri Arias <aecheverri.1995@gmail.com>
2020-11-09 22:25:30 -0500

    .

--- js/script.js
+++ js/script.js
@@ -1,10 +1,12 @@
 var ratio = 0.745;
 var height = 0;
+var width = 0;
+//Arreglar ancho porque la altura debe ser toda la pantalla. Hacer automatico para cada section con misma altura
 $(document).ready(function () {
     var inicio = document.getElementById("inicio");
     var fotos = document.getElementById("fotografias");
     var width = $("#inicio").outerWidth();
-    console.log(width);
+    //console.log(width);
     height = width * ratio;
     inicio.setAttribute("style", "height: " + height + "px");
     fotos.setAttribute("style", "height: " + height + "px");
@@ -16,5 +18,5 @@ $(window).resize(function () {
     height = width * ratio;
     inicio.removeAttribute("style");
     inicio.setAttribute("style", "height: " + height + "px");
-    console.log("...");
+    //console.log("...");
 });
