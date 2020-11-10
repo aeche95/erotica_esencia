e835e9b28434ae104cf119658ea40fb69577f013
Andres Echeverri Arias <aecheverri.1995@gmail.com>
2020-11-09 21:23:22 -0500

    Vertical v0.2

--- js/script.js
+++ js/script.js
@@ -1,16 +1,20 @@
 var ratio = 0.745;
+var height = 0;
 $(document).ready(function () {
     var inicio = document.getElementById("inicio");
-    var width = window.innerWidth;
-    var height = width * ratio;
+    var fotos = document.getElementById("fotografias");
+    var width = $("#inicio").outerWidth();
+    console.log(width);
+    height = width * ratio;
     inicio.setAttribute("style", "height: " + height + "px");
+    fotos.setAttribute("style", "height: " + height + "px");
 });
 
 $(window).resize(function () {
     var inicio = document.getElementById("inicio");
     var width = window.innerWidth;
-    var height = width * ratio;
-    inicio.removeAttribute("style")
+    height = width * ratio;
+    inicio.removeAttribute("style");
     inicio.setAttribute("style", "height: " + height + "px");
-    console.log("...")
+    console.log("...");
 });
