0b4ec1b90bb3b094982ca56671002ebbf33e3a03
Andres Echeverri Arias <aecheverri.1995@gmail.com>
2020-11-12 13:09:49 -0500

    Arreglos centrar

--- js/script.js
+++ js/script.js
@@ -1,19 +1,36 @@
 //Arreglar ancho porque la altura debe ser toda la pantalla. Hacer automatico para cada section con misma altura
-function setHeight() {
+function setWidthAndHeight() {
     var ratio = 0.745;
     var h = window.innerHeight;
     var w = h / ratio;
-    console.log("Height: "+h);
-    console.log("Width: " + w); 
+    //console.log("Height: "+h);
+    //console.log("Width: " + w); 
     $("section").css({ "height": h + "px" });
-    $("#inicio").css({ "width": w + "px" });
+    $("#inicio").css({ "width": window.innerWidth + "px" });
+    $("#centrar").css({ "width": w + "px", "height": h+"px" });
     $(".obj img, .obj h3").css({ "width": "600px", "height": "400px"});
 }
 
+function centrar() {
+    var ancho_ventana = window.innerWidth;
+    var ancho_elemento_str = $("#inicio").css("width");
+    var ancho_elemento = ancho_elemento_str.substr(0,ancho_elemento_str.length-2)
+    var desplazar = (ancho_ventana - ancho_elemento) / 2;
+    console.log("Ventana: " + ancho_ventana);
+    console.log("Elemento: " + ancho_elemento);
+    console.log("Offset: "+desplazar);
+    $("#inicio").css({ "position": "relative", "left": desplazar + "px" });
+}
+
 $(document).ready(function () {
-    setHeight();
-});
+    setWidthAndHeight();
+    //centrar();
+    $('html, body, *').mousewheel(function (e, delta) {
+        this.scrollLeft -= (delta*10);
+        e.preventDefault();
+    });
+   });
 
 $(window).resize(function () {
-    setHeight();
+    setWidthAndHeight();
 });
