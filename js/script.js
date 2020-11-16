dcc69edad0b57ed6aeb1853657ccdc8da8cee5c3
Andres Echeverri Arias <aecheverri.1995@gmail.com>
2020-11-15 22:54:31 -0500

    + imagenes y videos

--- js/script.js
+++ js/script.js
@@ -8,7 +8,11 @@ function setWidthAndHeight() {
     $("section").css({ "height": h + "px" });
     $("#inicio").css({ "width": window.innerWidth + "px" });
     $("#centrar").css({ "width": w + "px", "height": h+"px" });
-    $(".obj img, .obj h3").css({ "width": "640px", "height": "480px"});
+    $(".obj img").css({ "width": "640px", "height": "480px" });
+    var iframe_height_str = $("iframe").css("height");
+    var iframe_height = iframe_height_str.substr(0, iframe_height_str.length - 2);
+    console.log(iframe_height);
+    $("iframe").css({ "width": iframe_height*1.77778})
 }
 
 function centrar() {
