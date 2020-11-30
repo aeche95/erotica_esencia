b171c9e057e8cda8043a31bb6035f69743b95e7a
Andres Echeverri Arias <aecheverri.1995@gmail.com>
2020-11-29 22:31:18 -0500

    fix body width

--- js/script.js
+++ js/script.js
@@ -1,3 +1,7 @@
+var mismoAncho = false;
+var anchoAnterior;
+var anchoActual;
+var intervalo;
 function setWidthAndHeight() {
     var ratio = 0.745;
     var h = window.innerHeight;
@@ -10,37 +14,66 @@ function setWidthAndHeight() {
     var iframe_height_str = $("iframe").css("height");
     var iframe_height = iframe_height_str.substr(0, iframe_height_str.length - 2);
     //console.log(iframe_height);
-    $("iframe").css({ "width": iframe_height * 1.77778 });
-    //TODO Ajustar el ancho de body automaticamente
-    //$("body").css({ "width": w_body +"px"});
+    $("iframe").css({ "width": iframe_height * 1.77778 }); 
 }
 
-function modal(img) {
-    var modal = $("#myModal");
-    var image = img;
-    var modalImg = $("#modalImg");
-    modal.style.display = "block";
-    modalImg.src = image.src;
-    
+function getBodyWidth() {
+    var widthInicio = $("#inicio")[0].scrollWidth;
+    //console.log("Inicio: " + widthInicio);
+    var widthContenido = $("#contenido")[0].scrollWidth;
+    //console.log("Contenido: " + widthContenido);
+    var bodyWidth = widthInicio + widthContenido + 1;
+    //console.log("Ancho total: " + bodyWidth);
+    anchoAnterior = anchoActual;
+    anchoActual = bodyWidth;
+    if (anchoActual === anchoAnterior) {
+        document.getElementsByTagName("body")[0].style.width = bodyWidth + "px";
+        mismoAncho = true;
+    }
+    else {
+        mismoAncho = false;
+    }
+    checkInterval();
+}
+
+function checkInterval() {
+    if (mismoAncho) {
+        window.clearInterval(intervalo);
+        //console.log("Clear");
+    }
 }
 
 $(document).ready(function () {
+
     setWidthAndHeight();
+   
     $('html, body, *').mousewheel(function (e, delta) {
-        this.scrollLeft -= (delta * 30);
+        this.scrollLeft -= (delta * 40);
         e.preventDefault();
     });
+
+    var modal = $("#myModal");
     $(".obj").click(function () {
-        modal(this);
+        var image = this;
+        console.log(image.src);
+        var modalImg = document.getElementById("modalImg");
+        modal.css({ "display": "block" });
+        modalImg.src = image.src;
     });
     var span = document.getElementsByClassName("close")[0];
 
     // When the user clicks on <span> (x), close the modal
     span.onclick = function () {
-        modal.style.display = "none";
+        modal.css({ "display": "none" });
     };
+    intervalo = window.setInterval(getBodyWidth, 500);
+  
 });
 
 $(window).resize(function () {
     setWidthAndHeight();
+    getBodyWidth();
+    getBodyWidth();
 });
+
+ 
